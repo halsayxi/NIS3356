4db60cc00d23d116c72c39c9829594dc20dcc6be
halsayxi <halsayxi@sjtu.edu.cn>
2023-12-27 00:52:51 +0800

    first commit

--- test.py
+++ test.py
@@ -1,61 +1,55 @@
-# -*- coding:utf-8 -*-
+import ssc
+from kmp import VatiantKMP
 
-import time
-from model import BertTextModel_last_layer
-from utils import MyDataset
-from transformers import BertTokenizer
-from torch.utils.data import DataLoader
-import torch
-from config import parsers
-from colorama import Fore, Style
+SIMILARITY_THRESHOLD = 0.6
+SSC_ENCODE_WAY = 'ALL'  # 'ALL','SOUND','SHAPE'
 
-def load_model(model_path, device):
-    model = BertTextModel_last_layer().to(device)
-    model.load_state_dict(torch.load(model_path))
-    model.eval()
-    return model
+def make_all_china(string):
+    for i in range(0, len(string)):
+        if string[i] < u'\u4e00' or string[i] > u'\u9fa5':  # 判断是否是汉字，在isalpha()方法之前判断
+            string.replace(string[i], '', 1)
 
-def text_class_name(index, text, pred, args, output_file):
-    results = torch.argmax(pred, dim=1)
-    results = results.cpu().numpy().tolist()
-    classification = open(args.classification, "r", encoding="utf-8").read().split("\n")
-    classification_dict = dict(zip(range(len(classification)), classification))
+def is_all_china(string):
+    for i in range(0, len(string)):
+        if string[i] < u'\u4e00' or string[i] > u'\u9fa5':  # 判断是否是汉字，在isalpha()方法之前判断
+            return False
+    return True
+
+ssc.getHanziStrokesDict()
+ssc.getHanziStructureDict()
+ssc.getHanziSSCDict()
+
+def detect_sensitive_words(index, content):
+    variabt_word = set()
+    chi_word_ssc = ssc.getSSC(content, SSC_ENCODE_WAY)
     
-    with open(output_file, 'a', encoding='utf-8') as file:
-        if len(results) != 1:
-            for i in range(len(results)):
-                file.write(f"索引：{index[i]}\t文本：{text[i]}\t预测的类别为：{classification_dict[results[i]]}\n")
-        else:
-            file.write(f"索引：{index[0]}\t文本：{text}\t预测的类别为：{classification_dict[results[0]]}\n")
+    with open('data/filter.txt', 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            chi_word = line.strip()
+            if not is_all_china(chi_word):
+                continue
+            chi_word_ssc_filter = ssc.getSSC(chi_word, SSC_ENCODE_WAY)
+            
+            kmp = VatiantKMP(SIMILARITY_THRESHOLD)
+            kmp.indexKMP(chi_word_ssc, chi_word_ssc_filter, SSC_ENCODE_WAY)  # 主串S、模式串T
+            
+            for i in kmp.startIdxRes:
+                variabt_word.add(chi_word[i:i+len(chi_word)])
+    return variabt_word
 
-def pred_from_txt(args, model, device, start, output_file):
-    txt_path = 'input/topic_217915_posts.txt'  # 替换为你的 txt 文件路径
-    with open(txt_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-    indices = [line.strip().split('\t')[0] for line in lines]
-    texts = [line.strip().split('\t')[1] for line in lines]
-
-    x = MyDataset(texts, labels=None, with_labels=False)
-    xDataloader = DataLoader(x, batch_size=len(texts), shuffle=False)
-
-    for batch_index, batch_con in enumerate(xDataloader):
-        batch_con = tuple(p.to(device) for p in batch_con)
-        pred = model(batch_con)
-        text_class_name(indices, texts, pred, args, output_file)
-
-    end = time.time()
-    print(f"耗时为：{end - start} s")
+def main(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as input_f, open(output_file, 'w', encoding='utf-8') as output_f:
+        output_f.write("索引\t内容\t预测的类别\n")
+        for line in input_f:
+            index, content = line.strip().split('\t')
+            variabt_word = detect_sensitive_words(index, content)
+            if not variabt_word or '' in variabt_word:
+                output_f.write(f"{index}\t{content}\t预测的类别为：legal\n")
+            else:
+                output_f.write(f"{index}\t{content}\t预测的类别为：illegal，敏感词包括：{variabt_word}\n")
 
 if __name__ == "__main__":
-    start = time.time()
-    args = parsers()
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    model = load_model(args.save_model_best, device)
+    input_file = 'input/topic_217915_posts_2.txt'  # Replace with your input file path
+    output_file = 'output/output_217915_2.txt'  # Replace with your desired output file path
+    main(input_file, output_file)
 
-    output_file = 'output/output_217915.txt'
-    with open(output_file, 'w', encoding='utf-8') as file:
-        file.write("索引\t文本\t模型预测结果\n")
-
-    pred_from_txt(args, model, device, start, output_file)
-
-    print(f"预测结果已写入 {output_file}")
