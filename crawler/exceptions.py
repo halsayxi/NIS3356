e11de978130a20f056e5ac563a86bc26ac55d57d
halsayxi <halsayxi@sjtu.edu.cn>
2023-12-27 20:27:43 +0800

    initialize

--- crawler/exceptions.py
+++
@@ -1,7 +0,0 @@
-class ResponseDataFormatErrorException(Exception):
-    def __init__(self) -> None:
-        super().__init__('Response data format error! Please check whether cookies are correctly set.')
-
-class TooManyOperationsException(Exception):
-    def __init__(self) -> None:
-        super().__init__('Too many operations')
