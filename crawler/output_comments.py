a07691a77a0ec6171ef39811c285857b0e676f5f
Gym1013 <94842730+Gym1013@users.noreply.github.com>
2023-12-28 00:23:08 +0800

    增加读取实时评论数的功能

--- crawler/output_comments.py
+++ crawler/output_comments.py
@@ -2,6 +2,7 @@ import sys
 import os
 from bs4 import BeautifulSoup
 from typing import List
+import tkinter as tk
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
@@ -12,10 +13,16 @@ from crawler.models.topic import Topic
 from crawler.models.post import Post
 
 def get_topic_posts(cli: Client, topic_id: int) -> List[str]:
-    topic: Topic = cli.get_single_topic(topic_id)
-    posts_contents = []
+    # Create a new Tkinter window
+    window = tk.Tk()
+    window.title("读取进度")
+    label = tk.Label(window, text="已读取的评论数: 0")
+    label.pack()
 
-    for post_id in topic.post_stream.stream:
+    topic: Topic = cli.get_single_topic(topic_id)
+    posts_contents = [topic.title]
+
+    for i, post_id in enumerate(topic.post_stream.stream):
         post_detail: Post = cli.retrieve_single_post(post_id)
         # parse HTML and extract text
         soup = BeautifulSoup(post_detail.cooked, 'lxml')
@@ -24,8 +31,15 @@ def get_topic_posts(cli: Client, topic_id: int) -> List[str]:
         text_content = text_content.replace('\n', ' ').replace('\r', ' ')
         posts_contents.append(text_content)
 
-    return posts_contents
+        # update the label with the number of posts read
+        label.config(text=f"已读取的评论数: {i+1}")
+        # update the Tkinter window to reflect the changes
+        window.update()
 
+    # Close the window after reading all posts
+    window.destroy()
+
+    return posts_contents
 
 def output_comments(topic_id):
     with open('crawler/cookies.txt', 'r') as f:
